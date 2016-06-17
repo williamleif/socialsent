9f60d450f989b987f8aabc8b0b0398f535c621f0
williamleif <wleif@stanford.edu>
2016-06-17 10:10:25 -0700

    Added missing examples.

---
+++ example.py
@@ -0,0 +1,25 @@
+import seeds
+import lexicons
+from polarity_induction_methods import random_walk
+from evaluate_methods import binary_metrics
+from representations.representation_factory import create_representation
+
+if __name__ == "__main__":
+    print "Evaluting SentProp with 100 dimensional GloVe embeddings"
+    print "Evaluting only binary classification performance on General Inquirer lexicon"
+    lexicon = lexicons.load_lexicon("inquirer", remove_neutral=True)
+    pos_seeds, neg_seeds = seeds.hist_seeds()
+    embeddings = create_representation("GIGA", "data/example_embeddings/glove.6B.100d.txt",
+        set(lexicon.keys()).union(pos_seeds).union(neg_seeds))
+    eval_words = [word for word in embeddings.iw
+            if not word in pos_seeds 
+            and not word in neg_seeds]
+    # Using SentProp with 10 neighbors and beta=0.99
+    polarities = random_walk(embeddings, pos_seeds, neg_seeds, beta=0.99, nn=10,
+            sym=True, arccos=True)
+
+    acc, auc, avg_per  = binary_metrics(polarities, lexicon, eval_words)
+    print "Accuracy with best threshold: {:0.2f}".format(acc)
+    print "ROC AUC: {:0.2f}".format(auc)
+    print "Average precision score: {:0.2f}".format(auc)
+
---
+++ example.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# This script downloads and runs SentProp with 100 dimensional glove
+# embeddings on only positive/negative words in the inquirer corpus
+cd data
+mkdir example_embeddings
+cd example_embeddings
+curl -o glove_embeddings.zip http://nlp.stanford.edu/data/glove.6B.zip
+unzip glove_embeddings.zip
+rm glove_embeddings.zip glove.6B.50d.txt glove.6B.300d.txt glove.6B.200d.txt
+python example.py
