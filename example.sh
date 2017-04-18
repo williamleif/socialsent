490537ea08e2adda24fcd7165e7b9909d1f4b6c1
Haoyu Yang <FrankMN@users.noreply.github.com>
2017-04-18 20:17:09 +0800

    Update example.sh
    URL was modified to https

--- example.sh
+++ example.sh
@@ -5,7 +5,7 @@
 cd data
 mkdir example_embeddings
 cd example_embeddings
-curl -o glove_embeddings.zip http://nlp.stanford.edu/data/glove.6B.zip
+curl -o glove_embeddings.zip https://nlp.stanford.edu/data/glove.6B.zip
 unzip glove_embeddings.zip
 rm glove_embeddings.zip glove.6B.50d.txt glove.6B.300d.txt glove.6B.200d.txt
 python example.py
